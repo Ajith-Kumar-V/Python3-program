262c97467b08f2c55b652db0cd5935ed999782a9
Ajith Kumar V <54568246+Ajith-Kumar-V@users.noreply.github.com>
2019-12-10 15:11:29 +0530

    Update Quick_sort.py

--- Quick_sort.py
+++ Quick_sort.py
@@ -8,3 +8,19 @@ for i in range(0,len(Lis)):
   print(Lis)
   pivot=Lis[0]
 print(Lis)
+##############################
+def sorts():
+  less = []
+  equal = []
+  greater = []
+  if len(arrays) > 1:
+    pivot = arrays[0]
+    for x in arrays:
+      if x < pivot:
+        less.append(x)
+      if x == pivot:
+        equal.append(x)
+      if x > pivot:
+        greater.append(x)
+arrays=[int(x) for x in input().split()]
+sorts(arrays)
