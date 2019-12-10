4dd589b94a1a559faf1bc9a7d35e5087c85372f9
Ajith Kumar V <54568246+Ajith-Kumar-V@users.noreply.github.com>
2019-12-10 10:21:46 +0530

    Update Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,6 +1,6 @@
 List=[int(x) for x in input().split()]
-for i in range(1,len(List)):
-  for j in range(len(List)):
-    if List[i]<List[j]:
-	    List[i],List[j]=List[j],List[i]
+for i in range(len(List)):
+  for j in range(1,len(List)):
+    if List[j]<List[j-1]:
+	    List[j],List[j-1]=List[j-1],List[j]
 print(List)
